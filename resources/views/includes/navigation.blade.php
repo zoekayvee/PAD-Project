b1a31efa4c60589a8382e2c184892e412cae8adc
gelloguiam <acguiam@gmail.com>
2015-12-22 00:30:40 +0800

    added User Controller

--- resources/views/includes/navigation.blade.php
+++ resources/views/includes/navigation.blade.php
@@ -7,13 +7,13 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
         </button>
-        <a class="navbar-brand" href="#">YSES Tracker</a>
+        <a class="navbar-brand" href="/">YSES Tracker</a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav navbar-right">
-                <li><a href="#">*Name*</a></li>
+                <li><a href="/profile">*Name*</a></li>
                 <li><a href="#">Logout</a></li>
-                <li><a href="#">Help</a></li>
+                <li><a href="/users">Help</a></li>
             </ul>
             <form class="navbar-form navbar-right">
                 <input type="text" class="form-control" placeholder="Search...">
