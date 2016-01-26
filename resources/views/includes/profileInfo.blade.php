f09a78a09b51ebed046c37f48a233adaaaee9ada
magijoylarin <magijoylarin@gmail.com>
2016-01-26 15:37:08 +0800

    Bar chart on profile info of OAH

--- resources/views/includes/profileInfo.blade.php
+++ resources/views/includes/profileInfo.blade.php
@@ -1,4 +1,17 @@
 @section('includes')
+    <?php
+      $names = array();
+      $progress = array();
+      foreach ($committees as $nama)
+      {
+        array_push($names, $nama->name);
+        array_push($progress, $nama->progress);
+      }
+
+      $names = json_encode($names);
+      $progress = json_encode($progress);
+    ?>
+
     <link rel="stylesheet" type="text/css"
         href="{{ asset('/css/task_progress.css') }}">
     <link rel="stylesheet" type="text/css"
@@ -29,45 +42,57 @@
 
   <div class="col-md-9" id="home-left">
     <div class="table-responsive col-md-12">
-      @foreach($heads_comm as $head)
-        <tr>
-        @foreach ($committees as $committee)
-          @if($head->comm_id == $committee->id)
-            @foreach($events as $event)
-              @if($event->id == $committee->event_id)
-                <h3 class="section-title">{{$committee->name}} Progress</h3>
-                <?php
-                  $curr_comm = $committee->progress;
-                ?>
-                <div class="progress">
-                    <div class="progress-bar progress-bar-striped" role="progressbar" aria-valuenow="{{ $curr_comm }}" aria-valuemin="0" aria-valuemax="100" style="width: {{ $curr_comm }}%"><span class="">{{ $curr_comm }}%</span></div>
-                </div>
-              @endif
-            @endforeach <!-- endforeach events  -->
-          @endif
-        @endforeach <!-- endforeach all_comm -->
-        </tr>
-      @endforeach <!-- endforeach heads_comm -->
+      @if($curr_event->oah_id == $user->id)
+        <h3 class="section-title">{{$curr_event->title}} : {{$curr_event->theme}}</h3>
+        <div id="canvas-holder">
+          <canvas id="chart-area"/>
+        </div> 
 
-      @foreach ($mem_comm as $member)
-        <tr>
-          @foreach($committees as $committee)
-              @if($committee->id == $member->comm_id)
-                  @foreach($events as $event)
-                      @if($event->id == $committee->event_id)
-                        <h3>{{$committee->name}} Progress</h3>
-                        <?php
-                          $curr_comm = $committee->progress;
-                        ?>
-                        <div class="progress">
-                            <div class="progress-bar progress-bar-striped" role="progressbar" aria-valuenow="{{ $curr_comm }}" aria-valuemin="0" aria-valuemax="100" style="width: {{ $curr_comm }}%"><span class="">{{ $curr_comm }}%</span></div>
-                        </div>
-                      @endif
-                  @endforeach <!-- endforeach events  -->
-              @endif  
+      @else
+        <h1>{{$curr_event->title}} : {{$curr_event->theme}}</h1>
+        <div>
+        @foreach($heads_comm as $head)
+          <tr>
+          @foreach ($committees as $committee)
+            @if($head->comm_id == $committee->id)
+              @foreach($events as $event)
+                @if($event->id == $committee->event_id)
+                  <h3 class="section-title">{{$committee->name}} Progress</h3>
+                  <?php
+                    $curr_comm = $committee->progress;
+                  ?>
+                  <div class="progress">
+                      <div class="progress-bar progress-bar-striped" role="progressbar" aria-valuenow="{{ $curr_comm }}" aria-valuemin="0" aria-valuemax="100" style="width: {{ $curr_comm }}%"><span class="">{{ $curr_comm }}%</span></div>
+                  </div>
+                @endif
+              @endforeach <!-- endforeach events  -->
+            @endif
           @endforeach <!-- endforeach all_comm -->
           </tr>
-      @endforeach <!-- endforeach mem_comm -->
+        @endforeach <!-- endforeach heads_comm -->
+
+        @foreach ($mem_comm as $member)
+          <tr>
+            @foreach($committees as $committee)
+                @if($committee->id == $member->comm_id)
+                    @foreach($events as $event)
+                        @if($event->id == $committee->event_id)
+                          <h3>{{$committee->name}} Progress</h3>
+                          <?php
+                            $curr_comm = $committee->progress;
+                          ?>
+                          <div class="progress">
+                              <div class="progress-bar progress-bar-striped" role="progressbar" aria-valuenow="{{ $curr_comm }}" aria-valuemin="0" aria-valuemax="100" style="width: {{ $curr_comm }}%"><span class="">{{ $curr_comm }}%</span></div>
+                          </div>
+                        @endif
+                    @endforeach <!-- endforeach events  -->
+                @endif  
+            @endforeach <!-- endforeach all_comm -->
+            </tr>
+        @endforeach <!-- endforeach mem_comm -->
+        </div>
+      @endif
+
       
       <br>
       <table class="table table-condensed table-hover table-border">
@@ -142,3 +167,28 @@
     </div>
   </div>
 </div>
+
+<script type="text/javascript">
+ 
+  var randomScalingFactor = function(){ return Math.round(Math.random()*100)};
+
+  var barChartData = {
+    labels :  {!! $names !!},
+    datasets : [
+      {
+        fillColor : "rgba(151,187,205,0.5)",
+        strokeColor : "rgba(151,187,205,0.8)",
+        highlightFill : "rgba(151,187,205,0.75)",
+        highlightStroke : "rgba(151,187,205,1)",
+        data : {!! $progress !!}
+      }
+    ]
+
+  }
+  window.onload = function(){
+    var ctx = document.getElementById("chart-area").getContext("2d");
+    window.myBar = new Chart(ctx).Bar(barChartData, {
+      responsive : true
+    });
+  }
+</script> 
